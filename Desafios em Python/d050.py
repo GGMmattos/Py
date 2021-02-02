a0609c2b9a5be713c06ff805b61c2d8637d89728
GGM <ra116843@uem.br>
2021-02-02 09:04:58 -0300

    Exercício em py

--- Desafios em Python/d050.py
+++ Desafios em Python/d050.py
@@ -1,1 +1,10 @@
-print('4')
+#Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+
+soma=0
+cont=0
+for i in range(1,7):
+    num=int(input('Informe o valor {} : '.format(i)))
+    if num % 2 == 0:
+        soma = soma + num
+        cont+=1
+print('Voce informpu {} numeros pares a soma dos mesmo é {}'.format(cont,soma))
