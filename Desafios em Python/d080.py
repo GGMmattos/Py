1d887b6f47932434bf85833b19d0eb743627c5fc
GGM <ra116843@uem.br>
2021-03-18 11:47:03 -0300

    Exercício em py

--- Desafios em Python/d080.py
+++ Desafios em Python/d080.py
@@ -1,3 +1,19 @@
+#Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista,
+#á na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
+
 num = list()
-for n in num:
-    num.append(int(input(f"Digite o {n} numero")))
+for n in range(0,5):
+    n1=(int(input(f"Digite um numero: ")))
+    if n == 0 or n1 > num[-1]:
+        num.append(n1)
+        print("Adicionado no final da lista...")
+    else:
+        pos=0
+        while pos < len(num):
+            if n1 <= num[pos]:
+                num.insert(pos, n1)
+                print(f"Adicionado na posição {pos}")
+                break
+            pos+=1
+
+print(f"Os valores digitados em orgem foram {num}")
