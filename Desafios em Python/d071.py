123fd39dbe347d2b947eb981c6a11657326897ba
GGM <ra116843@uem.br>
2021-03-27 10:46:31 -0300

    Exercício em py

--- Desafios em Python/d071.py
+++ Desafios em Python/d071.py
@@ -1,6 +1,7 @@
 #Crie um programa que simule o funcionamento de um caixa eletrônico.
 # No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro)
 # e o programa vai informar quantas cédulas de cada valor serão entregues
+
 valor = int(input("Informe o valor que deseja sacar: "))
 
 total = valor
---
+++ Desafios em Python/d101.py
@@ -0,0 +1,14 @@
+#Programa que entre com o ano e retorne o status da pessoa referente a votação, sendo que a resposta é o retorno de uma função
+
+def voto(ano):
+    from datetime import datetime   #economizamos memoria ao importar a biblioteca que sera usada apenas na função  apenas nela
+    idade = datetime.now().year - ano
+    if idade < 16:
+        return f"Voce tem {idade} anos: VOTO NEGADO"
+    elif 16 <= idade <= 18 or idade > 64:
+        return f"Voce tem {idade} anos: VOTO OPCIONAL" #Não precisamos do print
+    else:
+        return f"você tem {idade} anos: VOTO OBRIGATORIO"
+
+ano = int(input("Em que ano você nasceu?"))
+print(voto(ano))
