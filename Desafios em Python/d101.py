6d7420e4f910e26f043d0ed72da7a07c51ea167e
GGM <ra116843@uem.br>
2021-03-27 10:48:35 -0300

    Exercício em py

--- Desafios em Python/d101.py
+++ Desafios em Python/d101.py
@@ -1,7 +1,7 @@
 #Programa que entre com o ano e retorne o status da pessoa referente a votação, sendo que a resposta é o retorno de uma função
 
 def voto(ano):
-    from datetime import datetime   #economizamos memoria ao importar a biblioteca que sera usada apenas na função  apenas nela
+    from datetime import datetime    #economizamos memoria ao importar a biblioteca que sera usada apenas na função  apenas nela
     idade = datetime.now().year - ano
     if idade < 16:
         return f"Voce tem {idade} anos: VOTO NEGADO"
