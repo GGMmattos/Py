bafc713788743da857fbef75643bd17059a4568a
GGM <ra116843@uem.br>
2021-03-29 17:16:51 -0300

    Exercício em py

---
+++ Desafios em Python/d104.py
@@ -0,0 +1,16 @@
+#Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante ‘a função input() do Python, 
+# só que fazendo a validação para aceitar apenas um valor numérico. Ex: n = leiaInt(‘Digite um n: ‘)
+def leiaInt(msg):
+    while True:
+
+        a =str(input(msg))
+        if a.isnumeric():
+            a = int(a)
+        else:
+            print("ERRO, digite um número inteiro válido!!!")  
+    return a
+
+n = leiaInt("Digite um número: ")
+print(f"Você acabou de digitar o números: {n}")
+
+
