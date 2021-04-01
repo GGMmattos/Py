39ba057b45e3541b28eb6874608a06702dad7e01
GGM <ra116843@uem.br>
2021-04-01 08:57:46 -0300

    Exercício em py

---
+++ Desafios em Python/d106.py
@@ -0,0 +1,16 @@
+# Faça um mini-sistema que utilize o Interactive Help do Python. O usuário vai digitar o comando e o manual vai aparecer.
+#  Quando o usuário digitar a palavra ‘FIM’, o programa se encerrará
+
+def ajuda(com):
+    return help(com)
+
+while True:
+    com=str(input("Qual o comando? "))
+    if com.upper() == "FIM":
+        print("Programa finalizado!!! ")
+        break
+    else:
+        help(com)
+      
+        
+
