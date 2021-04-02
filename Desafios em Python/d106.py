ccdcf9186dced385486623a090966cb3a161b0a2
GGM <ra116843@uem.br>
2021-04-02 11:17:23 -0300

    Exercícios em py

--- Desafios em Python/d106.py
+++ Desafios em Python/d106.py
@@ -14,3 +14,4 @@ while True:
       
         
 
+    
---
+++ Desafios em Python/d107/moeda.py
@@ -0,0 +1,15 @@
+def metade (n):
+    res = n/2
+    return res
+
+def dobro (n):
+    res = n*2
+    return res
+
+def aumentar(n,p):
+    res =  n+(n * p/100)            #formula da porcentagem simplificada
+    return res
+
+def  diminuir(n,p):
+    res = n-(n * p/100)
+    return res
