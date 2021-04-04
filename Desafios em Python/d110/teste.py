3576d302b0137bd069db9801b76a75a350869b99
GGM <ra116843@uem.br>
2021-04-04 11:24:02 -0300

    Exercício em py

---
+++ Desafios em Python/d110/teste.py
@@ -0,0 +1,8 @@
+#Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(),
+# que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
+
+import moeda
+
+n=int(input("Informe o valor: "))
+moeda.resumo(n)
+
