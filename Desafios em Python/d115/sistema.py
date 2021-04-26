a0557665c0fe8fa8c48cde23032cfcc9745640c0
GGM <ra116843@uem.br>
2021-04-25 22:57:51 -0300

    Exercício em py

--- Desafios em Python/d115/sistema.py
+++ Desafios em Python/d115/sistema.py
@@ -1,3 +1,5 @@
+#Elaboração de um sistema que crie e cadastre  pessoas em um arquivo.
+
 from lib.interface import *
 from lib.arquivo import *
 from time import  sleep
@@ -9,10 +11,14 @@ if not arquivoExiste(arq):
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa' , 'Sair do Sistema'])
     if  resposta == 1:
-        #opção de listar o conteudo de um arquivo!
+        #opção de listar o conteudo de um arquivo
         lerArquivo(arq)
     elif resposta == 2:
-        cabecalho("Opção 2")
+        #opcão cadastrar nova pessoa
+        cabecalho("NOVO CADASTRO")
+        nome =  str(input("Nome: "))
+        idade = leiaint("Idade: ")
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
         cabecalho("Saindo do sistema, até logo!")
         break
