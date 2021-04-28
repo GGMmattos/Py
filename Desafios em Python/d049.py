28e2dd6fdcef7bf1e81f5e5c9a87449f135c2ab3
Gabriel Matos <73392609+GGMmattos@users.noreply.github.com>
2021-04-28 20:56:14 -0300

    Delete d049.py

--- Desafios em Python/d049.py
+++
@@ -1,6 +0,0 @@
-#Mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
-
-num = int(input("informe um numero para fazer a tabuada do mesmo > "))
-
-for i in range (0,11):
-    print(' {} X {:2} > {}'.format(num,i, num*i))
