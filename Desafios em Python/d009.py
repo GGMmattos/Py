46865ca4c4f41ee0a82d82d8621c82a4f1adb475
Gabriel Matos <73392609+GGMmattos@users.noreply.github.com>
2021-04-28 20:45:16 -0300

    Delete d009.py

--- Desafios em Python/d009.py
+++
@@ -1,5 +0,0 @@
-#Tabuada de um numero informado
-
-n=int(input('Informe um numero para a tabuada'))
-
-print('\n\n {} X 1 = {} \n {} x 2 = {} \n {} x 3 = {} \n {} x 4 = {} \n {} x 5 = {} \n {} x 6 = {} \n {} x 7 = {} \n {} x 8 = {} \n {} x 9 = {} \n {} x 10 = {} \n'.format(n,n*1, n,n*2, n,n*3, n,n*4,n,n*5,n,n*6,n,n*7,n,n*8, n,n*9, n,n*10))
