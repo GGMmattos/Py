941ce32156f66e9ae352b4f0e2a34d6f5d6235d3
Gabriel Matos <73392609+GGMmattos@users.noreply.github.com>
2021-04-28 20:56:06 -0300

    Delete d051.py

--- Desafios em Python/d051.py
+++
@@ -1,9 +0,0 @@
-#Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
-
-F=int(input("Informe o primeiro termo: "))
-R=int(input("Qual é a razão? "))
-
-decimo=F+((10-1)*R) #CALCULO DO TERMO N-1
-for i in range (F,decimo+R,R):
-    print('{}'.format(i), end=(' - '))
-print("ACABOU")
