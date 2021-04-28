b402dd5f50338f96ad485258112a50f9d3a8641b
Gabriel Matos <73392609+GGMmattos@users.noreply.github.com>
2021-04-28 20:45:08 -0300

    Delete d029.py

--- Desafios em Python/d029.py
+++
@@ -1,9 +0,0 @@
-#Radar eletronico
-
-vel = float(input('Informe sua velocidade: '))
-
-if vel <= 80:
-    print("Tenha um bom dia dirija com seguraça")
-else: 
-    calc=(vel-80)*7
-    print('Voce esta acima do limite de velocidade e será multado no valor de R${:.2f} !!!'.format(calc))
