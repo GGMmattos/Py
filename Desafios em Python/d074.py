f3b772990748fc16181865e0787e4f430ed740a6
Gabriel Matos <73392609+GGMmattos@users.noreply.github.com>
2021-04-28 21:00:49 -0300

    Delete d074.py

--- Desafios em Python/d074.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-
-n = (randint(0,10),randint(0,10),randint(0,10),randint(0,10),randint(0,10))
-
-print(f"Os numeros que sorteei não {n}")
-
-print(f'O maior valor sorteado foi {max(n)}')
-
-print(f'O menor valor sorteado foi {min(n)}')
