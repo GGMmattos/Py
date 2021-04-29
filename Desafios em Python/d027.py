1ceb8cfde190676388e4cf12334234b543322758
Gabriel Matos <73392609+GGMmattos@users.noreply.github.com>
2021-04-28 21:02:47 -0300

    Delete d027.py

--- Desafios em Python/d027.py
+++
@@ -1,7 +0,0 @@
-#Exercício Python 27: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
-
-n = str(input('Informe seu nome: ')).strip()
-nome = n.split()
-print('Seu primeiro nome é: {} '.format(nome[0]))
-
-print("Seu ultimo nome: {}".format(nome[len(nome)-1]))
