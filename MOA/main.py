639b139d13a5f5b1394820ecfacb98fc2313472b
Gabriel Matos <73392609+GGMmattos@users.noreply.github.com>
2022-09-28 07:22:19 -0300

    Update main.py
    versão funcional

--- MOA/main.py
+++ MOA/main.py
@@ -1,39 +1,47 @@
 import numpy as np
+
 grafo = []
 dados = []
 vertice = list()
 
 
 def vertex(dimensao):
+    for i in (range(dimensao)):
+        vetor = input()
+        dados = vetor.split()
+        grafo = [int(dados[1]), int(dados[2])]
+        if i == 'EOF':
+            return 0
+        else:
+            vertice.append(grafo)
 
-  for i in (range(dimensao)):
-    vetor = input()
-    dados = vetor.split()
-    grafo = [int(dados[1]), int(dados[2])]
-    if i == 'EOF':
-      return 0
-    else:
-      vertice.append(grafo)
 
 def closer():
-  distTotal = 0
-  C =[]
-  atual = vertice[0]
-  menorDist = 10000000
 
-  while len(C) != dimensao:
-    for k in vertice:
-      if k not in C:
-        ver1 = np.array((atual))
-        ver2 = np.array(k)
-        calc = np.linalg.norm(ver1 - ver2)
-        if calc < menorDist:
-          menorDist = calc
-          atual = k
+    C = []
+    i = vertice[0]
+    m = 0
+    nn = 0
+    menorDist = float('Inf')
+    Dtotal = 0
+    while len(C) != dimensao:
+        for j in vertice:
+            ver1 = np.array(i)
+            if j not in C:
+                ver2 = np.array(j)  # passa por todos os k vertices
+                calc = np.linalg.norm(ver1 - ver2)  # realiza o calcula da distancia de atual  e K
+                if calc < menorDist:
+                  menorDist = calc
+                  nn =j
+        i = nn
+        Dtotal += menorDist
+        menorDist = float('Inf')
+        C.append(i)
+    ver3 = np.array((C[-1]))
+    ver4 = np.array(vertice[0])
+    m = np.linalg.norm(ver3 - ver4)
 
-    distTotal = distTotal + menorDist
-    C.append(atual)
-  return distTotal
+    return Dtotal + m
 
 
 name = input()
