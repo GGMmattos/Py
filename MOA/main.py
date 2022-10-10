e9d868874512c48491d98f4a5e70bf811b2813cd
Gabriel Matos <73392609+GGMmattos@users.noreply.github.com>
2022-10-10 07:07:12 -0300

    Update main.py

--- MOA/main.py
+++ MOA/main.py
@@ -58,17 +58,15 @@ def calc(sol):
     ver4 = np.array(sol[0])
     last_one1 = np.linalg.norm(ver3 - ver4)
     resp = calc +last_one1
-    print(resp)
     return resp
 
-def opt(lista):
+def opt(a):
+    perm = permutations(a)
     melhor = 0
-    for i in range(0, dimension - 1, 1):
-        for j in range(0, dimension, 1):
-            lista[i], lista[j] = lista[j], lista[i]
-            aux = calc(lista)
-            if aux < trivial:
-                melhor = aux
+    for i in list(perm):
+        aux = calc(i)
+        if aux < trivial:
+            melhor = aux
     return melhor
 
 name = input()
@@ -81,11 +79,5 @@ dimension = int(float((dimension[dimension.find(':') + 2:])))
 
 vertex(dimension)
 trivial = closer(vertice)
-print(a)
-print("----------------------------")
 melhora = opt(a)
-print(melhora)
-
-perm = permutations(a)
-for i in list(perm):
-    print(i)
+print(f'{melhora}')
